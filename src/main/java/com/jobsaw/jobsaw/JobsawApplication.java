a7e8d23604f28b19b96ef1a6cd19fdd3326e63f9
Victor Florescu <victor.s.florescu@gmail.com>
2026-03-15 12:47:26 +0200

    spring initializr

---
+++ src/main/java/com/jobsaw/jobsaw/JobsawApplication.java
@@ -0,0 +1,13 @@
+package com.jobsaw.jobsaw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobsawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobsawApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jobsaw/jobsaw/JobsawApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobsaw.jobsaw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobsawApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
